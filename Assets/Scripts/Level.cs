a34ad2068515b2de63aa3fd389376c1750965951
Shavits <s.shavit4@gmail.com>
2020-12-26 14:44:53 +0200

    added power ups and obstacles
    still need to implement biggerplatform

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,24 +9,33 @@ public class Level : MonoBehaviour
 {
 
     
-    public Transform SapwnLocation;
+    public Transform[] SapwnLocations;
     public Transform person;
+    public Transform Throwable;
+    public Transform ThrowLocation;
     public Text ScoreText;
     public GameObject GameOverScreen;
     public GameObject WinScreen;
     public float SpawnTimerMax = 5f;
-    public int NumOfPeople = 5; // Number of people to spawn this level
+
+    public int[] order;// order of elements in the level
 
     public static event Action<bool> OnGameOver;
 
 
+    private const int SLOWMOTION = 3;
+    private const int BIGGERPLATFORM = 4;
+    private const int OBSTACLE = 5;
+
+
+    private int NumOfPeople = 5; // Number of people to spawn this level
     private float spawnTimer;
     private float score;
     private int peopleSpawned;
     private static Level instance;
-    private List<Transform> people;
+    private List<PowerUp> powerUps;
     private State state;
-
+    private int idx; //Keeps track of which elemnt we look at in the level order
 
     private enum State
     {
@@ -47,18 +56,22 @@ public class Level : MonoBehaviour
         spawnTimer = 2f;
         score = 0;
         peopleSpawned = 0;
+        idx = 0;
+        NumOfPeople = CountPeople();
         ScoreText.text = ScoreString();
         state = State.Playing;
-        people = new List<Transform>();
+        state = State.Playing;
+        powerUps = new List<PowerUp>();
         GameOverScreen.SetActive(false);
         WinScreen.SetActive(false);
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        if(state == State.Playing && peopleSpawned < NumOfPeople)
+        /*if(state == State.Playing && peopleSpawned < NumOfPeople)
         {
             if (spawnTimer < 0)
             {
@@ -67,18 +80,78 @@ public class Level : MonoBehaviour
             }
 
             spawnTimer -= Time.deltaTime;
+        }*/
+
+
+        if(state == State.Playing)
+        {
+            if (idx < order.Length)
+            {
+                int current = order[idx];
+                if (spawnTimer < 0)
+                {
+                    Debug.Log(idx);
+                    if(current <= 2)
+                    {
+                        if(current == 1)
+                        {
+                            SpawnPerson(SapwnLocations[0]);
+
+                        }
+                        else
+                        {
+                            SpawnPerson(SapwnLocations[1]);
+                        }
+                        spawnTimer = SpawnTimerMax;
+                        idx++;
+                    }
+                    else if(current >= 2)
+                    {
+                        if(current == SLOWMOTION)
+                        {
+                            SpawnThrowable("SlowMotion");
+                        }else if (current == BIGGERPLATFORM)
+                        {
+                            SpawnThrowable("BiggerPlatform");
+                        }else if(current == OBSTACLE)
+                        {
+                            SpawnThrowable("Obstacle");
+                        }
+                        spawnTimer = SpawnTimerMax/2;
+                        idx++;
+                    }
+                }
+
+                spawnTimer -= Time.deltaTime;
+            }
+        }
+
+
+        //Update all powerup timers and remove those who are finished
+        for (int i = powerUps.Count - 1; i >= 0; i--)
+        {
+            if (powerUps[i].UpdateTimer(Time.deltaTime))
+            {
+                powerUps.RemoveAt(i);
+            }
         }
 
 
     }
 
-    private void SpawnPerson()
+    private void SpawnPerson(Transform spawnLoc)
     {
         Transform newPerson = Instantiate(person);
-        newPerson.position = SapwnLocation.position;
-        people.Add(newPerson);
+        newPerson.position = spawnLoc.position;
         peopleSpawned++;
         
+    }
+
+    private void SpawnThrowable(string type)
+    {
+        Transform newThrow = Instantiate(Throwable);
+        newThrow.GetComponent<Throwable>().SetType(type);
+        newThrow.position = ThrowLocation.position;
     }
     
     private void Win()
@@ -104,12 +177,30 @@ public class Level : MonoBehaviour
         }
     }
 
+    public void ApplyPowerUp(string type)
+    {
+        powerUps.Add(new PowerUp(type));
+    }
+
     public int GetPeopleSpawned()
     {
         return peopleSpawned;
     }
     
+    private int CountPeople()
+    {
+        int res = 0;
+        for(int i = 0; i < order.Length; i++)
+        {
+            if (order[i] <= 2)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
 
+    //Switches state to GameOver and stops all the people, powerups and firemen from moving. also activates gameover screen
     public void GameOver()
     {
         state = State.GameOver;
@@ -126,5 +217,67 @@ public class Level : MonoBehaviour
     public void Restart()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+
+    private class PowerUp
+    {
+        private float timer;
+        private float timerMax = 5f;
+        private Type type;
+
+        private enum Type
+        {
+            SlowMotion,
+            BiggerPlatform
+        }
+
+        public PowerUp(string type)
+        {
+            switch (type)
+            {
+                case "SlowMotion":
+                    this.type = Type.SlowMotion;
+                    break;
+                case "BiggerPlatform":
+                    this.type = Type.BiggerPlatform;
+                    break;
+            }
+            timer = timerMax;
+            Activate();
+        }
+
+        private void Activate()
+        {
+            switch (type.ToString())
+            {
+                case "SlowMotion":
+                    Time.timeScale = 0.5f;
+                    break;
+                case "BiggerPlatform":
+                    Debug.Log("startBigger");
+                    break;
+            }
+        }
+
+        //Checks if the timer is finished and if so cancels it's effect and returns true
+        public bool UpdateTimer(float deltaTime)
+        {
+            timer -= deltaTime;
+            if (timer < 0)
+            {
+                switch (type.ToString())
+                {
+                    case "SlowMotion":
+                        Time.timeScale = 1f;
+                        return true;
+                    case "BiggerPlatform":
+                        Debug.Log("stopBigger");
+                        return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private State state;
+
+    public Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+    public Sprite[] spriteArray;
+    public float MinXForce;
+    public float MaxXForce;
+    public float YForce;
+    public Type type;
+
+    private enum State
+    {
+        Playing,
+        GameOver
+    }
+
+    public enum Type
+    {
+        SlowMotion,
+        BiggerPlatform,
+        Obstacle
+    }
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        InitSpriteDict();
+    }
+
+    void Start()
+    {
+        state = State.Playing;
+        Level.OnGameOver += GameOver;
+        Init();
+    }
+
+    private void Init()
+    {
+        float xForce = Random.Range(MinXForce, MaxXForce);
+        rb.AddForce(new Vector2(xForce, YForce), 0f);
+    }
+
+    public void SetType(string type)
+    {
+
+        switch (type)
+        {
+            case "SlowMotion":
+                this.type = Type.SlowMotion;
+                break;
+            case "BiggerPlatform":
+                this.type = Type.BiggerPlatform;
+                break;
+            case "Obstacle":
+                this.type = Type.Obstacle;
+                break;
+        }
+
+        sr.sprite = sprites[type];
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(state == State.Playing)
+        {
+            if (collision.tag == "Player")
+            {
+                if(type != Type.Obstacle)
+                {
+                    Level.GetInstance().ApplyPowerUp(type.ToString());
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    Level.GetInstance().GameOver();
+                }
+            }
+            else if (collision.tag == "Ground")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+    private void GameOver(bool gameOver)
+    {
+        state = State.GameOver;
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void OnDestroy()
+    {
+        Level.OnGameOver -= GameOver;
+    }
+
+    private void InitSpriteDict()
+    {
+        sprites.Add("SlowMotion", spriteArray[0]);
+        sprites.Add("BiggerPlatform", spriteArray[1]);
+        sprites.Add("Obstacle", spriteArray[2]);
+    }
+}
