cf9822b821d13a8da9db4b308fc73ec5f3890790
Shavits <s.shavit4@gmail.com>
2021-01-16 21:51:54 +0200

    added tutorial

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class LevelData : MonoBehaviour
 {
+
+    public GameObject Tutorial;
+
+    private void Awake()
+    {
+        HideTutorial();
+    }
     public void StartLevel()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    public void ShowTutorial()
+    {
+        Tutorial.SetActive(true);
+    }
+
+    public void HideTutorial()
+    {
+        Tutorial.SetActive(false);
     }
 }
